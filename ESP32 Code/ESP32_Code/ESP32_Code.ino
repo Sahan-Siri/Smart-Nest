aacd212be5f4c529ea8dd1cefd6eca1a40929ebe
SahanSiri <sahan00siriwardena@gmail.com>
2024-05-12 22:30:16 +0530

    Update

--- ESP32 Code/ESP32_Code/ESP32_Code.ino
+++ ESP32 Code/ESP32_Code/ESP32_Code.ino
@@ -30,17 +30,17 @@ String uid;
 String Stat;
 
 void setup(){
-  Serial.begin(115200);
+  //Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
+  //Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
+    //Serial.print(".");
     delay(300);
   }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+  //Serial.println();
+  //Serial.print("Connected with IP: ");
+  //Serial.println(WiFi.localIP());
+  //Serial.println();
   pinMode(22,OUTPUT);
   /* Assign the api key (required) */
   config.api_key = API_KEY;
@@ -49,11 +49,11 @@ void setup(){
   /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
   uid = auth.token.uid.c_str();
-  Serial.println(uid);
+ // Serial.println(uid);
   Firebase.begin(&config, &auth);
   uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
+  //Serial.print("User UID: ");
+  //Serial.println(uid);
   signupOK = true;
 
   /* Assign the callback function for the long running token generation task */
@@ -65,43 +65,43 @@ void loop(){
   if (Firebase.RTDB.getString(&fbdo, "sandunmax422/Kitchen Plug 02/Switch")){
     String switchValue = fbdo.stringData();
     switchValue = switchValue.substring(2, switchValue.length() - 2); // Remove extra quotes
-    Serial.println("Switch Value: " + switchValue);
+    //Serial.println("Switch Value: " + switchValue);
     if (switchValue=="On"){
-      Serial.println("Ok");
+      //Serial.println("Ok");
       digitalWrite(22,HIGH);
     }
     else{
-      Serial.println("GO");
+      //Serial.println("GO");
       digitalWrite(22,LOW);
     }
   }
   else {
-    Serial.println("FAILED to get Switch value");
-    Serial.println("REASON: " + fbdo.errorReason());
+    //Serial.println("FAILED to get Switch value");
+    //Serial.println("REASON: " + fbdo.errorReason());
   }
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
     // Write an Int number on the database path test/int
     if (Firebase.RTDB.setFloat(&fbdo, "sandunmax422/Kitchen Plug 02/Power",int(0.01+random(5,25)))){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
+      //Serial.println("PASSED");
+      //Serial.println("PATH: " + fbdo.dataPath());
+      //Serial.println("TYPE: " + fbdo.dataType());
     }
     else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
+      //Serial.println("FAILED");
+      //Serial.println("REASON: " + fbdo.errorReason());
     }
     count++;
     
     // Write an Float number on the database path test/float
     if (Firebase.RTDB.setFloat(&fbdo, "sandunmax422/Kitchen Plug 02/Voltage",int( 0.01 + random(228,235)))){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
+      //Serial.println("PASSED");
+      //Serial.println("PATH: " + fbdo.dataPath());
+      //Serial.println("TYPE: " + fbdo.dataType());
     }
     else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
+      //Serial.println("FAILED");
+      //Serial.println("REASON: " + fbdo.errorReason());
     }
   }
 }
